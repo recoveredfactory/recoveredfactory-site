40480209a01272109a0b987e2b51cf063ce3476d
David Eads <davideads@gmail.com>
2026-01-20 00:46:39 -0500

    Switch support links to Stripe live

--- apps/web/src/lib/support.ts
+++ apps/web/src/lib/support.ts
@@ -1,7 +1,7 @@
 export const SUPPORT_LINKS = {
-  monthly12: 'https://buy.stripe.com/test_9B628semw6lPbMdeKa9IQ00',
-  monthly75: 'https://buy.stripe.com/test_7sYaEY3HS7pTdUl7hI9IQ01',
-  oneTime: 'https://buy.stripe.com/test_4gM28s7Y8dOh4jL31s9IQ02',
+  monthly12: 'https://buy.stripe.com/bJe14oblegwncEAcn2fAc02',
+  monthly75: 'https://buy.stripe.com/3cIfZi0GAeofcEAcn2fAc01',
+  oneTime: 'https://buy.stripe.com/5kQ8wQble5RJ5c82MsfAc00',
 };
 
 export const SUPPORT_CONTACT_URL = 'mailto:davideads@recoveredfactory.net';
